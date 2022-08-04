44017141827e1f9ed7571f8ecd3bd901169c476f
thienanhlove <nguyenquocanhtruc@gmail.com>
2022-08-04 09:32:54 +0700

    init file project

---
+++ saleservice/src/test/java/com/anhnguyen/saleservice/SaleserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhnguyen.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
